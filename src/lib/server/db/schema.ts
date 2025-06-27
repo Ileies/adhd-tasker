17480a52746c309ed380ee59ae8cc52ea531e2b0
Ileies <admin@ileies.de>
2025-06-27 14:28:47 +0200

    removed userId from db: email suffices

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,8 +2,7 @@ import { Priority, TaskStatus } from '../../types';
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('user', {
-	id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
-	email: text('email').notNull(),
+	email: text('email').notNull().primaryKey(),
 	resetTime: integer('reset_time').default(0).notNull() // day time to reset the tasks
 	// Further settings can be added here
 });
@@ -12,7 +11,7 @@ export const tasks = sqliteTable('tasks', {
 	id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
 	title: text('title').notNull(),
 	description: text('description').notNull(),
-	userId: integer('user_id').references(() => users.id, { onDelete: 'cascade' }).notNull(),
+	email: integer('user_id').references(() => users.email, { onDelete: 'cascade' }).notNull(),
 	startTime: integer('start_time').notNull(),
 	duration: integer('duration').notNull(),
 	priority: integer('priority').$type<Priority>().default(Priority.Medium).notNull(),
