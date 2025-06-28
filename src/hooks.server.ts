7a74a9834d3747fd4cd4b3758f76cfb927a1fb64
Ileies <admin@ileies.de>
2025-06-28 06:07:06 +0200

    Made Google Login optional

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,24 @@
 import { type Handle } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
-import { login } from '$lib/server';
 import { users } from '$lib/server/db/schema';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	console.log('Handling request:', event.url.pathname + event.url.search);
 
-	if (!event.url.pathname.startsWith('/callback')) {
-		const token = event.cookies.get('token');
-		if (!token) login(event);
-
+	const token = event.cookies.get('token');
+	if (token) {
 		const email = (await (await fetch("https://openidconnect.googleapis.com/v1/userinfo", { headers: { Authorization: `Bearer ${token}` } })).json())?.email;
-
-		if (!email) login(event);
-		event.locals.user = await db.query.users.findFirst({
-			where: (users, { eq }) => eq(users.email, email),
-		});
-		if (!event.locals.user) event.locals.user = await db.insert(users).values({email}).returning();
+		if (email) {
+			event.locals.user = await db.query.users.findFirst({
+				where: (users, { eq }) => eq(users.email, email),
+			});
+			if (!event.locals.user) {
+				event.locals.user = await db.insert(users).values({ email }).returning();
+			}
+		} else {
+			event.cookies.set('token', '', { path: '/', maxAge: -1 });
+		}
 	}
+
 	return resolve(event);
 };
